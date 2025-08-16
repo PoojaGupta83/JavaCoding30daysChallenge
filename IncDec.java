045964e53e3082f8aa4ad4da114e20451394f503
PoojaGupta <56184524+PoojaGupta83@users.noreply.github.com>
2025-08-16 21:48:35 +0530

    Day 9: Program for Increment Decrement Operator

---
+++ IncDec.java
@@ -0,0 +1,15 @@
+class IncDec{
+public static void main(String arfs[]){
+int x=5,y=0,z=0,a=0;
+x++;
+System.out.println(x);
+y=x++;
+z=++x;
+a=2*x++*3*++y;
+System.out.println(y);
+System.out.println(z);
+
+System.out.println(a);
+}
+}
+
