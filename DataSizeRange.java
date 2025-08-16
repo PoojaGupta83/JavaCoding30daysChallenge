a295e775e0c443eb27fdf2c573ede2f204e5ca60
PoojaGupta <56184524+PoojaGupta83@users.noreply.github.com>
2025-08-16 20:43:14 +0530

    Day4-Program to check data type range

---
+++ DataSizeRange.java
@@ -0,0 +1,12 @@
+
+class DataSizeRange{
+public static void main(String args[]){
+System.out.println("Integer Minimum Range="+Integer.MIN_VALUE);
+System.out.println("Integer Maximum Range="+Integer.MAX_VALUE);
+System.out.println("Integer Bytes="+Integer.BYTES);
+System.out.println("Float Minimum Range="+Float.MIN_VALUE);
+System.out.println("Float Maximum Range="+Float.MAX_VALUE);
+System.out.println("Float Byte="+Float.BYTES);
+
+}}
+
