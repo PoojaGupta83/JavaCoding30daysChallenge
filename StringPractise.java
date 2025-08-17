42a18ca5b66ef24e24329969b1244d4e40ddd903
PoojaGupta <56184524+PoojaGupta83@users.noreply.github.com>
2025-08-17 23:39:29 +0530

    Day 9: String Practise

---
+++ StringPractise.java
@@ -0,0 +1,14 @@
+class StringPractise{
+public static void main(String args[]){
+String str1="Java Program";
+System.out.println(str1);
+String str2=new String();
+System.out.println(str2);
+char c[]={'A','B','C','D'};
+String str3=new String(c);
+byte b[]={65,66,67,68};
+String str4=new String(b);
+System.out.println(str3);
+System.out.println(str4);
+}
+}
