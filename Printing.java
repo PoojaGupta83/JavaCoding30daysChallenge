2baa614a4965554525a49d5e7ea34b19394960c2
PoojaGupta <56184524+PoojaGupta83@users.noreply.github.com>
2025-08-17 23:32:44 +0530

    Day7: Printing

---
+++ Printing.java
@@ -0,0 +1,13 @@
+class Printing{
+public static void main(String r[]){
+int x=10;
+float f=20.0f;
+char c='A';
+String str="7gshs";
+System.out.println(x);
+System.out.printf("Hello %d %f %c\n",x,f,c);
+System.out.format("%5d",x);
+System.out.format("%f",f);
+System.out.printf("%s",str);
+}
+}
